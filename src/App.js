a0bb1f994cf34d2b0fc19452bc5a70ddd1899a41
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-01-04 14:50:20 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+const Parent =() => {
+  const userArray = [
+    {id:1, name:"ana"},
+    {id:2, name:"levani"},
+  ];
+
+  return (
+    <>
+      <Child userArray={userArray} isUserLoggedIn={true} />
+    </>
+  )
+};
+
+const Child = ({isUserLoggedIn, userArray}) => {
+  if (!isUserLoggedIn) {
+    return <h1>User in not loggedin</h1>
+  }
+
+  return (
+    <h1>
+      {userArray.length > 0 ? (
+        <>
+          {userArray.map((item) => {
+            return <h1 key={item.id}>{item.name}</h1>
+          })}
+        </>
+      )  : (
+        ""
+        )}
+    </h1>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parent />
     </div>
   );
 }
